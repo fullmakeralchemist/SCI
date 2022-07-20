5e859c78b16257fd076a15b9f5ab33256b6e3d03
fullmakeralchemist <makeralchemist@outlook.com>
2022-07-20 18:24:43 -0500

    first arduino

---
+++ Arduino Codes/excercise1Var/excercise1Var.ino
@@ -0,0 +1,28 @@
+/*
+ * Declaracion de variables
+ */
+
+int sum;
+
+byte var1 = 10;
+byte var2 = 8;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+  //SUMA
+  suma = var1 + var2;
+  Serial.print(res);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(1000); //Wait for 1000 miliseconds or one second
+  digitalWrite(13, LOW);
+  delay(1000); //Wait for 1000 miliseconds or one second
+}
