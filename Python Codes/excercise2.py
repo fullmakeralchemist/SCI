8aa9a064f3e3bb264ed0bf70dd6a44da99e94ca7
fullmakeralchemist <makeralchemist@outlook.com>
2022-07-20 20:54:05 -0500

    prueba

---
+++ Python Codes/excercise2.py
@@ -0,0 +1,9 @@
+#Vamos a nombrar la variable, 
+# el nombre debe ser descriptivo para saber que valor almacena, 
+# simbolo igual y el valor almacenado en la variable
+
+"""
+Vamos a nombrar la variable, 
+el nombre debe ser descriptivo para saber que valor almacena, 
+simbolo igual y el valor almacenado en la variable
+"""
