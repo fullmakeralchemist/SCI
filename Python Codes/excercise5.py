c01a90d34807ed89761141fe62fdbeef2950e87a
fullmakeralchemist <makeralchemist@outlook.com>
2022-07-20 22:13:26 -0500

    'excercesie5p'

--- Python Codes/excercise5.py
+++ Python Codes/excercise5.py
@@ -4,3 +4,30 @@
 una variable puede comenzar teniendo un tipo de dato y 
 cambiar en cualquier momento a otro tipo de dato. Por ejemplo:
 """
+
+valor = 'Curso de Python'
+
+valor = 10
+
+valor = 3.14
+
+valor = True
+
+print(valor)
+
+
+## Part II
+
+"""
+valor = 'Curso de Python'
+print(valor)
+
+valor = 10
+print(valor)
+
+valor = 3.14
+print(valor)
+
+valor = True
+print(valor)
+"""
