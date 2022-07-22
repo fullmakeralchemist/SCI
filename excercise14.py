b6e87a9c656e1ea1785061b609c8642a88b9a89e
fullmakeralchemist <makeralchemist@outlook.com>
2022-07-21 22:22:41 -0500

    'excercise14fixed'

--- excercise14.py
+++ excercise14.py
@@ -1,3 +1,4 @@
+#MODIFICAR LISTAS
 lista_cursos = ['Python', 'Java', 'Django', 'Flask', 'Ruby']
 lista_cursos_dos = ['C++', 'C', 'Matlab']
 print(len(lista_cursos))
@@ -12,3 +13,40 @@ print(len(lista_cursos))
 
 lista_cursos.remove('MongoDB')
 print(lista_cursos)
+
+"""
+lista_cursos = ['Python', 'Java', 'Django', 'Flask', 'Ruby']
+lista_cursos_dos = ['C++', 'C', 'Matlab']
+print(len(lista_cursos))
+
+lista_cursos.append('MongoDB')
+
+lista_cursos.insert(1, 'Rails')
+
+lista_cursos.extend(lista_cursos_dos)
+print(lista_cursos)
+print(len(lista_cursos))
+
+del lista_cursos[0]
+print(lista_cursos)
+"""
+
+
+#PART 3
+
+"""
+lista_cursos = ['Python', 'Java', 'Django', 'Flask', 'Ruby']
+lista_cursos_dos = ['C++', 'C', 'Matlab']
+print(len(lista_cursos))
+
+lista_cursos.append('MongoDB')
+
+lista_cursos.insert(1, 'Rails')
+
+lista_cursos.extend(lista_cursos_dos)
+print(lista_cursos)
+print(len(lista_cursos))
+
+lista_cursos.clear()
+print(lista_cursos)
+"""
